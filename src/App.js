7a7c791443a906d1d0c264fe5cccfa15172c3be3
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-07-25 17:18:36 +0800

    Change App into a stateless functional component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 import DataFetcher from "./DataFetcher";
 import Books from "./Books";
 import Authors from "./Authors";
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <DataFetcher endpoint="/books" render={props => <Books {...props} />} />
-        <DataFetcher
-          endpoint="/authors"
-          render={props => <Authors {...props} />}
-        />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <DataFetcher endpoint="/books" render={props => <Books {...props} />} />
+      <DataFetcher
+        endpoint="/authors"
+        render={props => <Authors {...props} />}
+      />
+    </div>
+  );
+};
 
 export default App;
