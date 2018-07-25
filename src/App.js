c22c8f9122e4ffbedcd6015289152ee31a13ff18
Sahil Bajaj <spinningarrow@users.noreply.github.com>
2018-07-25 13:46:16 +0800

    Show list of books from books-api

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      books: []
+    };
+  }
+
+  async getBooks() {
+    const response = await fetch("http://localhost:3000/books");
+    if (response.ok) {
+      const data = await response.json();
+      this.setState({
+        books: data
+      });
+    }
+  }
+
+  async componentDidMount() {
+    await this.getBooks();
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {this.state.books.map(book => {
+          return <li key={book._id}>{book.title}</li>;
+        })}
       </div>
     );
   }
